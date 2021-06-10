6e64f70f5f7705ce44807f94165454dc26a7dcda
Julin8 <65641337+Julin8@users.noreply.github.com>
2021-06-11 01:00:18 +0800

    Add files via upload

--- fastSellingDrinks/src/main/java/com/nju/fastSellingDrinks/config/DataSourceConfig.java
+++ fastSellingDrinks/src/main/java/com/nju/fastSellingDrinks/config/DataSourceConfig.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.DefaultResourceLoader;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 
